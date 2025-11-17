08a174e0f39cd6f770e15697b8ebb304d1c9d9c9
issx77 <issa7791100@gmail.com>
2025-11-17 11:59:29 +0100

    3commit

--- src/main/kotlin/org/ldv/monster/controller/admincontrollers/MainController.kt
+++ src/main/kotlin/org/ldv/monster/controller/admincontrollers/MainController.kt
@@ -1,4 +1,44 @@
 package org.ldv.monster.controller.admincontrollers
 
-class MainController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MainController (){
+
+
+    /**
+     * Méthode permettant d'afficher la page d'accueil de l'application.
+     * @return le chemin vers le template a partir du dossier ressources/templates (on ne marque pas le .html)
+     */
+    @GetMapping("/WebMonster")
+    fun home():String{
+        return "index"
+    }
+
+    @Controller
+    class MainController {
+
+        @GetMapping("/WebMonster/a-propos")
+        fun aPropos(): String {
+            return "pagesVisiteur/a-propos"
+        }
+
+        @GetMapping("/WebMonster/contact")
+        fun contact(): String {
+            return "pagesVisiteur/contact"
+        }
+
+        @GetMapping("/WebMonster/inscription")
+        fun inscription(): String {
+            return "pagesVisiteur/inscription"
+        }
+
+
+        @GetMapping("/WebMonster/rgpd")
+        fun rgpd(): String {
+            return "pagesVisiteur/rgpd"
+        }
+    }
+
 }
